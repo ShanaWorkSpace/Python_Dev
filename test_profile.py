5160a882d7dacb47f6c9582920a6775e9dad2c1f
Shana Yasmin S <shanayasminofficial@gmail.com>
2025-01-15 21:50:38 +0530

    Updated test Response codes

--- test_profile.py
+++ test_profile.py
@@ -1,7 +1,30 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app  # Assuming your FastAPI app is in 'main.py'
+from utils import log_test_result
 
+from main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost:5432/stud_profile_db_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Override the dependency to use the test database
+@pytest.fixture(scope="module")
+def db():
+    # Create the tables in the test database
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=engine)
 
 @pytest.fixture(scope="module")
 def client():
@@ -9,17 +32,18 @@ def client():
 
 
 def test_create_student_profile(client):
+
     # Valid profile creation
     new_profile = {
-        "name": "John Doe",
-        "email": "johndoe@example.com",
+        "name": "Jain",
+        "email": "johdoe@gmail.com",
         "phone": "1234567890",  # Optional field
-        "password": "Password123"
+        "password": "Password@123"
     }
     response = client.post("/profiles", json=new_profile)
-    assert response.status_code == 201
-    assert response.json()["name"] == "John Doe"
-    assert response.json()["email"] == "johndoe@example.com"
+    assert response.status_code == 200
+    assert response.json()["name"] == "Jain"
+    assert response.json()["email"] == "johdoe@gmail.com"
 
 
 def test_create_student_profile_missing_name(client):
@@ -65,6 +89,6 @@ def test_create_student_profile_invalid_password(client):
         "password": "short"
     }
     response = client.post("/profiles", json=new_profile)
-    assert response.status_code == 422
+    assert response.status_code == 400
 
 
